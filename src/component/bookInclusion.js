0dc8f3de532edabe4e9ace0d499de91f272a4436
Alexandre Fernandes <alex.silva250@hotmail.com>
2023-11-03 21:39:30 -0300

    feat: axios adding and final adjustments in front-end

--- src/component/bookInclusion.js
+++ src/component/bookInclusion.js
@@ -1,32 +1,40 @@
+import { useForm } from "react-hook-form";
+
 const BookInclusion = () => {
+    const { register, handleSubmit }= useForm();
+
+    const save = (fields) =>{
+        alert(JSON.stringify(fields))
+    }    
+
     return ( 
         <div className="container">
             <h4 className="fst-italic mt-3">Inclusão</h4>
-            <form>
+            <form onSubmit={handleSubmit(save)}>
                 <div className="form-group">
-                    <label htmlFor="title">Título:</label>
-                    <input type="text" className="form-control" id="title" required
-                        autoFocus/>
+                    <label htmlFor="Titulo">Título:</label>
+                    <input type="text" className="form-control" id="Titulo" required
+                        autoFocus {...register("Titulo")}/>
                 </div>
                 <div className="form-group mt-2">
-                    <label htmlFor="author">Autor:</label>
-                    <input type="text" className="form-control" id="author" required/>
+                    <label htmlFor="Autor">Autor:</label>
+                    <input type="text" className="form-control" id="Autor" required {...register("Autor")}/>
                 </div>
                 <div className="form-group mt-2">
-                    <label htmlFor="photo">URL da foto:</label>
-                    <input type="text" className="form-control" id="photo" required/>
+                    <label htmlFor="Foto">URL da foto:</label>
+                    <input type="text" className="form-control" id="Foto" required {...register("Foto")}/>
                 </div>
                 <div className="row mt-2">
                     <div className="col-sm-4">
                         <div className="form-group">
-                            <label htmlFor="year">Ano de Publicação:</label>
-                            <input type="number" className="form-control" id="year" required/>
+                            <label htmlFor="Ano">Ano de Publicação:</label>
+                            <input type="number" className="form-control" id="Ano" required {...register("Ano")}/>
                         </div>
                     </div>
                     <div className="col-sm-8">
                         <div className="form-group">
-                            <label htmlFor="price">Preço:</label>
-                            <input type="number" className="form-control" id="price" step="0.1" required/>
+                            <label htmlFor="Preco">Preço:</label>
+                            <input type="number" className="form-control" id="Preco" step="0.1" required {...register("Preco")}/>
                         </div>
                     </div>
                 </div>
